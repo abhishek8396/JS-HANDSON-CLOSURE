111dd62ae9e8092eec909eb419735176c9ede31e
anhishek1289@gmail.com <abhishek1289@gmail.com>
2022-12-23 09:05:26 +0530

    closure

---
+++ main.js
@@ -0,0 +1,15 @@
+function Counter() {
+    var counter = 0;
+
+    function IncreaseCounter() {
+        return counter += 1;
+    };
+
+    return IncreaseCounter;
+}
+
+var counter = Counter();
+console.log(counter()); 
+console.log(counter()); 
+console.log(counter()); 
+console.log(counter());
